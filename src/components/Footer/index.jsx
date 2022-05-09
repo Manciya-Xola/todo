cc559700fc78c56cee36eaf800c37ff82f619df1
Xola Manciya <xola.manciya@yoco.com>
2022-05-09 12:04:52 +0200

    Create Layout, Header, Nav and Footer components
    - used position for puting the footer at the bottom and header at the top

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Footer.module.css'
+function Footer() {
+  return (
+    <div className={styles.container}>
+      <h5>Todo List © 2022</h5>
+    </div>
+  )
+}
+
+export default Footer;
