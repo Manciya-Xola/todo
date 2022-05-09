74ae1df146377da942a0251868a76ce46881d942
Xola Manciya <xola.manciya@yoco.com>
2022-05-09 12:50:27 +0200

    Create basic pages and routing
    - Changed some styling to accommodate the children on the layout"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 import Layout from './components/Layout';
-
+import Home from './pages/Home';
+import Todo from './pages/Todo';
+import About from './pages/About';
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Layout/>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/todo" element={<Todo/>}/>
+          </Routes>
+        </Layout>
       </BrowserRouter>
     </div>
   );
